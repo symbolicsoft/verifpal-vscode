ed17260b68c2b4eea54f8e8d0581383f0962d74f
Nadim Kobeissi <nadim@symbolic.software>
2020-04-22 17:12:13 +0200

    Formatting

--- src/HoverProvider.ts
+++ src/HoverProvider.ts
@@ -5,22 +5,22 @@ import * as vscode from 'vscode';
 import VerifpalLib from './VerifpalLib';
 
 export default class HoverProvider {
-    provideHover(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken): Promise<any> {
-        const wordPosition = document.getWordRangeAtPosition(position);
-        if (!wordPosition) return new Promise((resolve) => resolve());
-        const word = document.getText(wordPosition);
-        return VerifpalLib.getTypeAtPos(
-            document.getText(), document.uri.fsPath, position).then((typeAtPos: any) => {
-                // const beautifiedData = beautify(typeAtPos.type, { indent_size: 4 });
-                return new vscode.Hover([
-                    'Verifpal',
-                    { language: 'javascriptreact', value: `${word}` }
-                ]);
-            }).catch((e) => {
+	provideHover(
+		document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken): Promise<any> {
+		const wordPosition = document.getWordRangeAtPosition(position);
+		if (!wordPosition) return new Promise((resolve) => resolve());
+		const word = document.getText(wordPosition);
+		return VerifpalLib.getTypeAtPos(
+			document.getText(), document.uri.fsPath, position).then((typeAtPos: any) => {
+				// const beautifiedData = beautify(typeAtPos.type, { indent_size: 4 });
+				return new vscode.Hover([
+					'Verifpal',
+					{ language: 'javascriptreact', value: `${word}` }
+				]);
+			}).catch((e) => {
 
-            });
-    }
+			});
+	}
 }
--- src/VerifpalLib.ts
+++ src/VerifpalLib.ts
@@ -9,70 +9,70 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export default class VerifpalLib {
-    static execVerifpal(fileContents, filename, args) {
-        return new Promise((resolve, reject) => {
-            if (!fs.existsSync(filename)) {
-                resolve(undefined);
-            }
-            const cwd = path.dirname(filename);
-            let verifpalOutput = "";
-            let verifpalOutputError = "";
-            const verifpalProc = spawn(configDeterminePath(), args, { cwd: cwd });
-            verifpalProc.stdout.on('data', (data) => {
-                verifpalOutput += data.toString();
-            });
-            verifpalProc.stderr.on('data', (data) => {
-                verifpalOutputError += data.toString();
-            });
-            verifpalProc.on('exit', () => {
-                if (verifpalOutputError) {
-                    reject(verifpalOutputError);
-                } else {
-                    let result = verifpalOutput;
-                    if (verifpalOutput && verifpalOutput.length) {
-                        // result = JSON.parse(verifpalOutput);
-                        result = verifpalOutput;
-                    }
-                    resolve(result);
-                }
-            });
-            verifpalProc.stdin.write(fileContents);
-            verifpalProc.stdin.end();
-        });
-    }
+	static execVerifpal(fileContents, filename, args) {
+		return new Promise((resolve, reject) => {
+			if (!fs.existsSync(filename)) {
+				resolve(undefined);
+			}
+			const cwd = path.dirname(filename);
+			let verifpalOutput = "";
+			let verifpalOutputError = "";
+			const verifpalProc = spawn(configDeterminePath(), args, { cwd: cwd });
+			verifpalProc.stdout.on('data', (data) => {
+				verifpalOutput += data.toString();
+			});
+			verifpalProc.stderr.on('data', (data) => {
+				verifpalOutputError += data.toString();
+			});
+			verifpalProc.on('exit', () => {
+				if (verifpalOutputError) {
+					reject(verifpalOutputError);
+				} else {
+					let result = verifpalOutput;
+					if (verifpalOutput && verifpalOutput.length) {
+						// result = JSON.parse(verifpalOutput);
+						result = verifpalOutput;
+					}
+					resolve(result);
+				}
+			});
+			verifpalProc.stdin.write(fileContents);
+			verifpalProc.stdin.end();
+		});
+	}
 
-    static getTypeAtPos(fileContents: string, fileName, pos: vscode.Position) {
-        return VerifpalLib.execVerifpal(
-            fileContents,
-            fileName,
-            ['type-at-pos', '--json', '--pretty', '--path', fileName, pos.line + 1, pos.character + 1]);
-    }
+	static getTypeAtPos(fileContents: string, fileName, pos: vscode.Position) {
+		return VerifpalLib.execVerifpal(
+			fileContents,
+			fileName,
+			['type-at-pos', '--json', '--pretty', '--path', fileName, pos.line + 1, pos.character + 1]);
+	}
 
-    static getDiagnostics(fileContents: string, fileName: string): any {
-        return VerifpalLib.execVerifpal(
-            fileContents,
-            fileName,
-            ['status', '--json']);
-    }
+	static getDiagnostics(fileContents: string, fileName: string): any {
+		return VerifpalLib.execVerifpal(
+			fileContents,
+			fileName,
+			['status', '--json']);
+	}
 
-    static getAutocomplete(fileContents: string, fileName: string, pos: vscode.Position): any {
-        return VerifpalLib.execVerifpal(
-            fileContents,
-            fileName,
-            ['autocomplete', '--json', fileName, pos.line + 1, pos.character + 1]);
-    }
+	static getAutocomplete(fileContents: string, fileName: string, pos: vscode.Position): any {
+		return VerifpalLib.execVerifpal(
+			fileContents,
+			fileName,
+			['autocomplete', '--json', fileName, pos.line + 1, pos.character + 1]);
+	}
 
-    static getDefinition(fileContents: string, fileName: string, pos: vscode.Position): any {
-        return VerifpalLib.execVerifpal(
-            fileContents,
-            fileName,
-            ['get-def', '--json', fileName, pos.line + 1, pos.character + 1]);
-    }
+	static getDefinition(fileContents: string, fileName: string, pos: vscode.Position): any {
+		return VerifpalLib.execVerifpal(
+			fileContents,
+			fileName,
+			['get-def', '--json', fileName, pos.line + 1, pos.character + 1]);
+	}
 
-    static getCoverage(fileContents: string, fileName: string): any {
-        return VerifpalLib.execVerifpal(
-            fileContents,
-            fileName,
-            ['coverage', '--json', fileName]);
-    }
+	static getCoverage(fileContents: string, fileName: string): any {
+		return VerifpalLib.execVerifpal(
+			fileContents,
+			fileName,
+			['coverage', '--json', fileName]);
+	}
 }
