0f8d0b4c6bf486fd6fade7b600f1a12dbc5b2231
Nadim Kobeissi <nadim@symbolic.software>
2020-04-22 17:12:07 +0200

    Fix associations

--- src/extension.ts
+++ src/extension.ts
@@ -15,62 +15,54 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 const supportedLanguages = [
-    "javascriptreact",
-    "javascript"
+	"javascriptreact",
+	"javascript"
 ];
 
 let paramHintsEnable = false;
 
 export function activate(context: vscode.ExtensionContext) {
+	if (!configGetEnabled()) {
+		return false;
+	}
+	context.subscriptions.push(
+		vscode.languages.registerSignatureHelpProvider(
+			[
+				{ language: 'verifpal', scheme: 'file', pattern: '**/*vp*' }
+			],
+			new SignatureProvider(), '(', '.')
+	);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    if (!configGetEnabled()) {
-        return false;
-    }
-    // The registration needs to happen after a timeout because of 
-    context.subscriptions.push(
-        vscode.languages.registerSignatureHelpProvider(
-            [
-                { language: 'javascript', scheme: 'file', pattern: '**/*js*' },
-                { language: 'javascriptreact', scheme: 'file', pattern: '**/*js*' }
-            ],
-            new SignatureProvider(), '(', '.')
-    );
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider([
+			{ language: 'verifpal', scheme: 'file', pattern: '**/*vp*' }
+		],
+			new HoverProvider()));
 
-    context.subscriptions.push(
-        vscode.languages.registerHoverProvider([
-            { language: 'javascript', scheme: 'file', pattern: '**/*js*' },
-            { language: 'javascriptreact', scheme: 'file', pattern: '**/*js*' }
-        ],
-            new HoverProvider()));
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider([
+			{ language: 'verifpal', scheme: 'file', pattern: '**/*vp*' }
+		],
+			new AutocompleteProvider(), '.'));
 
-    context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider([
-            { language: 'javascript', scheme: 'file', pattern: '**/*js*' },
-            { language: 'javascriptreact', scheme: 'file', pattern: '**/*js*' }
-        ],
-            new AutocompleteProvider(), '.'));
+	context.subscriptions.push(
+		vscode.languages.registerDefinitionProvider([
+			{ language: 'verifpal', scheme: 'file', pattern: '**/*vp*' }
+		],
+			new DefinitionProvider())
+	);
 
-    context.subscriptions.push(
-        vscode.languages.registerDefinitionProvider([
-            { language: 'javascript', scheme: 'file', pattern: '**/*js*' },
-            { language: 'javascriptreact', scheme: 'file', pattern: '**/*js*' }
-        ],
-            new DefinitionProvider())
-    );
-
-    const coverage = new CoverageProvider(context.subscriptions);
-    const refreshCoverage = () => {
-        coverage.toggleDecorations();
-        coverage.refreshCoverage();
-    };
-    const showVerifpalPath = () => {
-        vscode.window.showInformationMessage(`Verifpal path set to:${configGetEnabled()}`);
-    }
-    vscode.commands.registerCommand('verifpal.coverage', refreshCoverage);
-    vscode.commands.registerCommand('verifpal.path', showVerifpalPath);
-    setupDiagnostics(context.subscriptions);
+	const coverage = new CoverageProvider(context.subscriptions);
+	const refreshCoverage = () => {
+		coverage.toggleDecorations();
+		coverage.refreshCoverage();
+	};
+	const showVerifpalPath = () => {
+		vscode.window.showInformationMessage(`Verifpal path set to: ${configGetEnabled()}`);
+	}
+	vscode.commands.registerCommand('verifpal.coverage', refreshCoverage);
+	vscode.commands.registerCommand('verifpal.path', showVerifpalPath);
+	setupDiagnostics(context.subscriptions);
 }
 
 // this method is called when your extension is deactivated
